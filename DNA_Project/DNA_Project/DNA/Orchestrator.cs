f2a0ee6d20be2b3dea622380ef82938477de1e36
PAULY <matthieu.pauly@sogeti.com>
2018-07-23 17:06:04 +0200

    commit de matthieu.

--- DNA_Project/DNA_Project/DNA/Orchestrator.cs
+++ DNA_Project/DNA_Project/DNA/Orchestrator.cs
@@ -1,22 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using DNA_Project.DNA;
+using DNA_Project.Helper;
 
 
 namespace DNA_Project.DNA
 {
     class Orchestrator
     {
-        String ipAddr;
-        int socketPort;
+        private List<Node> nodes;
+        private List<Chunck> chuncks;
+        private List<String> fileData;
+        private String filename { get; set; }
+        private List<String> dataLines;
 
-        List<Node> nodes;
-        List<Chunck> chuncks;
-        Byte [] fileData;
+        Socket SocketServer;
+        List<Socket> SocketsClient;
 
+        public Orchestrator()
+        {
+            // ONLY FOR DEBUG MODE
+            filename = @"C:\Users\mpauly\Documents\CESI RILA\Projet C# DNA\DNA-Data\genome-greshake.txt";
+
+            // Start server mode here to listen new connexion
+            SocketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            SocketServer.Bind(new IPEndPoint(LocalIPAddress.GetLocalIPAddressV2(), 54321));
+            SocketServer.Listen(1);
+
+            SocketServer.BeginAccept(new AsyncCallback(this.connexionAcceptCallback), SocketServer);
+
+        }
+
+
+        // Start work
+        public void Process()
+        {
+            dataLines = System.IO.File.ReadAllLines(filename).ToList();
+            
+            // Mapping & send datas to Nodes
+
+        }
+
+        private void connexionAcceptCallback(IAsyncResult asyncResult)
+        {
+            SocketsClient.Add(SocketServer.EndAccept(asyncResult));
+        }
     }
 
 
--- DNA_Project/DNA_Project/Helper/LocalIPAddress.cs
+++ DNA_Project/DNA_Project/Helper/LocalIPAddress.cs
@@ -22,5 +22,18 @@ namespace DNA_Project.Helper
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
         }
+
+        public static IPAddress GetLocalIPAddressV2()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
     }
 }
